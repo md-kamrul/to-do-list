071ac03f6dc8b8582a57998aa4befa883f62fda6
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-02-17 00:59:41 +0600

    feature - taking task from user done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,18 @@
 function addTask() { 
-    var task = document.getElementById('addTask');
+    let task = document.getElementById('addTask');
     
     if (task.value === "") {
         alert("You have to write about your task in the input box");
     }
     else { 
-        const taskText = document.getElementById('listContainerP');
-        taskText.innerText = task.value;
+        let listSection = document.getElementById('listSection');
+        let taskTextDiv = document.createElement('div');
+        let taskTextP = document.createElement('p');
+        taskTextP.innerText = task.value;
+        taskTextDiv.appendChild(taskTextP);
+        listSection.appendChild(taskTextDiv);
+
+        taskTextDiv.classList.add('list', 'uncheck');
     }
-    task.value = " ";
+    task.value = '';
 }
