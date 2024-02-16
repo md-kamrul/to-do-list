13cb4c1a3f70f05297a5df56b6cb2107c2986183
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-02-17 01:29:14 +0600

    adding task with close icon done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,6 +5,7 @@ function addTask() {
         alert("You have to write about your task in the input box");
     }
     else { 
+        // showing text
         let listSection = document.getElementById('listSection');
         let taskTextDiv = document.createElement('div');
         let taskTextP = document.createElement('p');
@@ -13,6 +14,13 @@ function addTask() {
         listSection.appendChild(taskTextDiv);
 
         taskTextDiv.classList.add('list', 'uncheck');
+        task.value = '';
+
+        // showing close icon
+        let closeIcon = document.createElement('div');
+        closeIcon.innerText = "notheing";
+        closeIcon.classList.add('close');
+        taskTextDiv.classList.add('flex', "flex-row", 'justify-between');
+        taskTextDiv.appendChild(closeIcon);
     }
-    task.value = '';
 }
