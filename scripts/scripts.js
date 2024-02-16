0125f094c62a2fe4bfd16d0e3bdbcd6022a8941c
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-02-17 03:20:23 +0600

    closing function running

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,24 @@
-function addTask() { 
+function addTask() {
     let task = document.getElementById('addTask');
-    
+
     if (task.value === "") {
         alert("You have to write about your task in the input box");
     }
-    else { 
+    else {
         // showing text
         let listSection = document.getElementById('listSection');
         let taskTextDiv = document.createElement('div');
-        let taskTextP = document.createElement('p');
+        let taskTextP = document.createElement('div');
+        taskTextDiv.setAttribute('id', 'taskTextDiv');
         taskTextP.innerText = task.value;
         taskTextDiv.appendChild(taskTextP);
         listSection.appendChild(taskTextDiv);
-
         taskTextDiv.classList.add('list', 'uncheck');
         task.value = '';
 
         // showing close icon
         let closeIcon = document.createElement('div');
-        closeIcon.innerText = "notheing";
+        closeIcon.innerText = "no..";
         closeIcon.classList.add('close');
         taskTextDiv.classList.add('flex', "flex-row", 'justify-between');
         taskTextDiv.appendChild(closeIcon);
